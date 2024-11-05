e3070423c4c9696f3be1d339aab073bbd22583fb
0tomz <tomazsa@.al.insper.br>
2024-11-05 15:14:02 -0300

    apaguei um negocinho 🤭

--- home.py
+++ home.py
@@ -9,11 +9,6 @@ st.logo('img/logo_aqui_perto.png', size="large", link=None, icon_image=None)
 
 components.html(html_code, width=None, height=1025, scrolling=False)
 
-if st.button('Entrar'):
-    st.switch_page('pages/1_login.py')
-
-if st.button('Cadastrar'):
-    st.switch_page('pages/2_cadastro.py')
 
 # st.sidebar.success(f"Logado como: {st.session_state['user_email']}")
 # if st.button("Sair"):
