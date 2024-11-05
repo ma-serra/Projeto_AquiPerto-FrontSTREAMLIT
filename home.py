9aed0a18bf6f1d0718ea47b7ae91b429edaefa5e
alec302 <alec.almeidaduarte@gmail.com>
2024-11-05 11:23:15 -0300

    imagem

--- home.py
+++ home.py
@@ -5,6 +5,6 @@ import streamlit.components.v1 as components
 with open("templates/home.html", "r", encoding="utf-8") as file:
     html_code = file.read()
 
-st.logo('img\logo_aqui_perto.png', size="large", link=None, icon_image=None)
+st.logo('img\logo.png', size="large", link=None, icon_image=None)
 
 components.html(html_code, width=None, height=1025, scrolling=False)
