ae5a629afa44479e38caf59ff8f835a795daaab4
MathLemoss <matheussl1@al.insper.edu.br>
2024-11-05 10:40:21 -0300

    Tela de home em streamlit

--- home.py
+++ home.py
@@ -1,17 +1,10 @@
 import streamlit as st
 import requests
+import streamlit.components.v1 as components
 
-api_url = "http://127.0.0.1:5000/home"
+with open("templates/home.html", "r", encoding="utf-8") as file:
+    html_code = file.read()
 
-# Código da home page aqui
+st.logo('img\logo_aqui_perto.png', size="large", link=None, icon_image=None)
 
-st.markdown(
-    """
-    <a href="/mapa" target="_self">
-        <button style="display:block; width:200px; height:50px;">
-            Ir para a próxima página
-        </button>
-    </a>
-    """,
-    unsafe_allow_html=True
-)
+components.html(html_code, width=None, height=1025, scrolling=False)
