654b617688c8bb27baa41c08fdd78027e32eb62f
EnzoDelloso <enzodo@al.insper.edu.br>
2024-11-05 08:53:20 -0300

    Home correta e local para adicionar estabelecimentos feito

--- home.py
+++ home.py
@@ -1,17 +1,25 @@
 import streamlit as st
 import requests
+import streamlit.components.v1 as components
 
 api_url = "http://127.0.0.1:5000/home"
 
 # Código da home page aqui
 
-st.markdown(
-    """
-    <a href="/mapa" target="_self">
-        <button style="display:block; width:200px; height:50px;">
-            Ir para a próxima página
-        </button>
-    </a>
-    """,
-    unsafe_allow_html=True
-)
+
+with open("templates/home.html", "r", encoding="utf-8") as file:
+    html_code = file.read()
+
+components.html(html_code, height=1500)
+
+
+# st.markdown(
+#     """
+#     <a href="/mapa" target="_self">
+#         <button style="display:block; width:200px; height:50px;">
+#             Ir para a próxima página
+#         </button>
+#     </a>
+#     """,
+#     unsafe_allow_html=True
+# )
